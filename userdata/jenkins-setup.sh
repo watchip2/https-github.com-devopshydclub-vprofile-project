4260a58662ccce983c704898ac482498ef22bebd
Yogesh-DJ <87028819+Yogesh-DJ@users.noreply.github.com>
2023-08-26 16:14:57 +0530

    Update jenkins-setup.sh
    working sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -2,8 +2,12 @@
 sudo apt update
 sudo apt install openjdk-8-jdk -y
 sudo apt install maven git wget unzip -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+        /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+         https://pkg.jenkins.io/debian-stable binary/ | sudo tee \ 
+         /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
-###
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
